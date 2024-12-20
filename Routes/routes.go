169d2d758cf9f66400a65e1dbb53b17f730995ca
nguruli <rjasper906@gmail.com>
2024-12-20 12:24:05 +0300

    API routes intergrations

--- Routes/routes.go
+++ Routes/routes.go
@@ -30,24 +30,24 @@ func Routes() {
 	r.POST("api/sendResetPasswordEmail/", controllers.SendResetPasswordEmail)
 	r.POST("api/reset-password/:token", controllers.ResetPassword)
 
-	// // product Routes
-	// r.GET("api/getProduct/:filename", controllers.GetProduct)
-	// r.GET("api/getProducts/:id", controllers.GetProducts)
-	// r.GET("api/getAllProducts/", controllers.GetAllProducts)
+	// product Routes
+	r.GET("api/getProduct/:filename", controllers.GetProduct)
+	r.GET("api/getProducts/:id", controllers.GetProducts)
+	r.GET("api/getAllProducts/", controllers.GetAllProducts)
 
-	// // Order Routes
-	// r.POST("api/addOrder/", controllers.AddOrder)
-	// r.GET("api/getOrder/:id", controllers.GetOrder)
-	// r.GET("api/getOrders/:id", controllers.GetOrders)
+	// Order Routes
+	r.POST("api/addOrder/", controllers.AddOrder)
+	r.GET("api/getOrder/:id", controllers.GetOrder)
+	r.GET("api/getOrders/:id", controllers.GetOrders)
 
-	// // Payment Routes
-	// r.POST("api/payOrder", controllers.AddPayment)
-	// r.GET("api/getPayment/:id", controllers.GetPayment)
-	// r.GET("api/getPayments/:id", controllers.GetPayments)
+	// Payment Routes
+	r.POST("api/payOrder", controllers.AddPayment)
+	r.GET("api/getPayment/:id", controllers.GetPayment)
+	r.GET("api/getPayments/:id", controllers.GetPayments)
 
-	// //Customer Routes
-	// r.POST("api/customerLogin/", controllers.CustomerAuthentication)
-	// r.POST("api/customerJoin/", controllers.AddCustomer)
+	//Customer Routes
+	r.POST("api/customerLogin/", controllers.CustomerAuthentication)
+	r.POST("api/customerJoin/", controllers.AddCustomer)
 
 	// User Routes
 	r.DELETE("api/deleteUser/:id", controllers.DeleteUser)
@@ -56,22 +56,22 @@ func Routes() {
 	r.PUT("api/updateUserPreferences/:id", controllers.UpdateUserPreferences)
 	r.GET("api/userPreferences/:id", controllers.GetUserPreferences)
 
-	// // product Routes
-	// r.POST("api/upload/", controllers.Upload)
-	// r.POST("api/insertProduct/:id", controllers.AddProduct)
-	// r.DELETE("api/deleteProduct/:id", controllers.DeleteProduct)
-	// r.PUT("api/updateProduct/:id", controllers.UpdateProduct)
+	// product Routes
+	r.POST("api/upload/", controllers.Upload)
+	r.POST("api/insertProduct/:id", controllers.AddProduct)
+	r.DELETE("api/deleteProduct/:id", controllers.DeleteProduct)
+	r.PUT("api/updateProduct/:id", controllers.UpdateProduct)
 
-	// // Order Routes
-	// r.DELETE("api/removeOrder/:id", controllers.RemoveOrder)
-	// r.PUT("api/updateOrder/:id", controllers.UpdateOrder)
+	// Order Routes
+	r.DELETE("api/removeOrder/:id", controllers.RemoveOrder)
+	r.PUT("api/updateOrder/:id", controllers.UpdateOrder)
 
-	// // Payment Routes
-	// r.DELETE("api/deletePayment/:id", controllers.DeletePayment)
-	// r.PUT("api/updatePayment/:id", controllers.UpdatePayment)
+	// Payment Routes
+	r.DELETE("api/deletePayment/:id", controllers.DeletePayment)
+	r.PUT("api/updatePayment/:id", controllers.UpdatePayment)
 
-	// //AzamPay Callback
-	// r.POST("api/callback", controllers.AzamPayCallbackHandler)
+	//AzamPay Callback
+	r.POST("api/callback", controllers.AzamPayCallbackHandler)
 
 	// //Logs Routes
 	// r.GET("api/logs", controllers.GetLogs)
